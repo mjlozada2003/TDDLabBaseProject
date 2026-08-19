76397d5b97e477b8ebcccc5055fc44208f4e0212
Maria Lozada <mariajesuslozada10@gmail.com>
2026-08-18 22:42:42 -0400

    feat: nueva boton multiplicación

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,27 @@
 import sumar from "./sumador";
+import multiplicar from "./multiplicador";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
+const multiplicarForm = document.querySelector("#multiplicar-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
-  event.preventDefault();
+  event.preventDefault(); //para no recargar la pagina
 
+  //por que se lee aqui adentro?
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});
+
+multiplicarForm.addEventListener("submit", (event) => {
+  event.preventDefault(); 
+
+  const firstNumber = Number.parseInt(first.value);
+  const secondNumber = Number.parseInt(second.value);
+
+  div.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
 });
